4d35f67b36d7b081c32ad44c8529b82e8724ff7b
kfcal <k.f.caldwell+datacraft@gmail.com>
2025-06-17 11:37:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ def erste_funktion():
 def wichtige_funktion():
     print("Andere tolle wichtige Sachen")
     print("Wichtige Datenbank Information")
+
+def nicht_so_wichtige_funktion():
+    print('Nur unwichtige Sachen")
