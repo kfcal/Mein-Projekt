9ddb5420b7519eb13543ea1301557df212b28b0e
Kevin-DataCraft <kevin@data-craft.de>
2025-06-17 07:46:46 +0200

    Revert "Wichtige Funktionen erstellt."
    This reverts commit 271d5c75e7ed90da2d03eb8d0fe899bc7585d1ea.

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
 def erste_funktion():
     print("Hello, World!")
-
-def wichtige_funktion():
-    erster_teil = "Das ist eine wichtige Information."
-    zweiter_teil = "Wichtige Datanbank Information"
-    dritter_teil = "Noch mehr wichtige Information"
-    vierter_teil = "Mehr Informationen!!"
-    print(erster_teil + ' ' + zweiter_teil + ' ' + dritter_teil + ' ' + vierter_teil)
